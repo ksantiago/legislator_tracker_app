4af5c3cc09e86c52b100f307d5c5cc2dda9afa16
Katherine Santiago <santiago.kat@gmail.com>
2014-02-03 17:22:16 -0500

    tried changing environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-# CongressTracker::Application.initialize!
-LegislatorTracker::Application.initialize!
+CongressTracker::Application.initialize!
